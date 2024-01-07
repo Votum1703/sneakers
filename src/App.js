0b3bc48819bcb1579121188a449fadd4ff696032
Shakes Peare <votum1703@gmail.com>
2024-01-07 23:03:10 +0300

    sneakers 4.1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Card from "./components/Card";
+import axios from "axios";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
-
 
 
 
@@ -11,38 +11,55 @@ import Drawer from "./components/Drawer";
 function App() {
   const [items, setItems] = React.useState([])
   const [cartItems, setCartItems] = React.useState([])
+  const [searchValue, setsearchValue] = React.useState("")
   const [cartOpened, setCartOpened] = React.useState(false)
 
 React.useEffect(()=>{
-  fetch('https://659403d01493b0116069ba63.mockapi.io/items').then(res =>{
-  return res.json()
-}).then(json =>{
-  setItems (json);
-})
+  axios.get('https://659403d01493b0116069ba63.mockapi.io/items').then(res =>{
+  setItems(res.data);
+});
+axios.get('https://659403d01493b0116069ba63.mockapi.io/cart').then(res =>{
+  setCartItems(res.data);
+});
 }, [])
 
+
+
+
 const onAddToCart = (obj) => {
-  setCartItems([...cartItems, obj])
+  axios.post('https://659403d01493b0116069ba63.mockapi.io/cart', obj)
+  setCartItems((prev) => [...prev, obj])
+};
+
+const onRemoveItem = (id) =>{
+  axios.delete(`https://659403d01493b0116069ba63.mockapi.io/cart/${id}`)
+  // setCartItems((prev) => [...prev, obj])
 }
-console.log(cartItems);
+
+const onChangeSearchInput = (event) => {
+  setsearchValue(event.target.value);
+};
 
 
   return (
      <div className="wrapper clear">
-{cartOpened && <Drawer items={cartItems} onClose = {() => setCartOpened(false)} />}
+{cartOpened && <Drawer items={cartItems} onClose = {() => setCartOpened(false)} onRemove={onRemoveItem} />}
  <Header onClickCart = {() => setCartOpened(true)} />
       <div class="content p-40">
        <div className="d-flex align-center mb-40 justify-between">
-        <h1>Все кроссовки</h1>
+        <h1>{searchValue ? `"Поиск по запросу: ${searchValue}"`: "Все кроссовки"}</h1>
         <div className="search-block d-flex">
           <img src="/img/search.svg" alt="Search"/>
-          <input placeholder="Поиск..."/>
+          {searchValue && <img onClick={() => setsearchValue ("")} className="clear cu-p" src="/img/btn-remove.svg" alt="Clear"/>}
+          <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
         </div>
        </div>
 <div className="d-flex flex-wrap">
  
-{items.map((item)=>(
+{items.filter(item=>item.title.toLowerCase().includes(searchValue.toLowerCase()))
+.map((item, index)=>(
   <Card 
+  key={index}
   title={item.title} 
   price ={item.price} 
   imageUrl={item.imageUrl}
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,10 +1,16 @@
-function Drawer({onClose, items = [] }){
+function Drawer({onClose, onRemove, items = []}){
+
+
+  
     return(
         <div className='overlay'>
         <div className='drawer'>
         <h2 className="d-flex justify-between mb-30 ">
           Корзина <img onClick={onClose} className='cu-p' src="/img/btn-remove.svg" alt="Close"/></h2>
-       
+
+     {
+      items.length > 0 ?         
+    <div>
         <div class="items">
       {items.map((obj) => (
               <div className="cartItem d-flex align-center mb-20">   
@@ -16,11 +22,11 @@ function Drawer({onClose, items = [] }){
             <p className='mb-5'>{obj.title}</p>
             <b>{obj.price}</b>
           </div>
-            <img className='removeBtn' src="/img/btn-remove.svg" alt="Remove"/>
+            <img onClick={() => onRemove(obj.id)} className='removeBtn' src="/img/btn-remove.svg" alt="Remove"/>
             </div>
       ))
       }
-    </div>
+    
     <div className="cartTotalBlock">
       <ul>
         <li className="d-flex">
@@ -36,6 +42,23 @@ function Drawer({onClose, items = [] }){
       </ul>
       <button className="greenButton">Оформить заказ <img src="/img/arrow.svg"alt='Arrow'/></button>     
           </div>
+    </div>
+
+
+    </div> :  <div class="cartEmpty d-flex align-center justify-center flex-column flex">
+  <img class="mb-20" width="120px" height="120px" src="/img/empty.svg" alt="emptyCart"/>
+  <h2>Корзина пустая</h2>
+  <p class='opacity-6'>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
+  <button class="greenButton">
+    <img src="/img/arrow.svg" alt="Arrow"/>Вернуться назад
+  </button>
+</div>
+     }
+
+         
+
+
+
         </div>
         </div>
     )
